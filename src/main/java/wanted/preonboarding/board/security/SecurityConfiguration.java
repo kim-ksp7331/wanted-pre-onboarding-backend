143da117b07810a1cb28ea315832d859a904de69
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-08-13 14:26:13 +0900

    Chore: docker compose 적용
    AWS EC2에서 mysql을 쉽게 사용하기 위해 docker compose를 사용했습니다.

---
+++ src/main/java/wanted/preonboarding/board/security/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package wanted.preonboarding.board.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+}

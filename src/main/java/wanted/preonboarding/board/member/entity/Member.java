29b6d87703b4f0e410fd10e26e85159c7a221fb8
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-08-14 10:19:07 +0900

    Fix: 이메일 중복 검사 추가
    로그인시 이메일은 유일해야 하는데 이를 누락했던 것을 수정했습니다.

--- src/main/java/wanted/preonboarding/board/member/entity/Member.java
+++ src/main/java/wanted/preonboarding/board/member/entity/Member.java
@@ -14,7 +14,9 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
 
     @Builder
--- src/main/java/wanted/preonboarding/board/member/service/MemberService.java
+++ src/main/java/wanted/preonboarding/board/member/service/MemberService.java
@@ -3,10 +3,14 @@ package wanted.preonboarding.board.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import wanted.preonboarding.board.exception.BusinessLogicException;
+import wanted.preonboarding.board.exception.ExceptionCode;
 import wanted.preonboarding.board.member.dto.MemberDTO;
 import wanted.preonboarding.board.member.entity.Member;
 import wanted.preonboarding.board.member.mapper.MemberMapper;
 import wanted.preonboarding.board.member.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +20,15 @@ public class MemberService {
     private final MemberMapper mapper;
 
     public Long createMember(MemberDTO memberDTO) {
+        existsMember(memberDTO.getEmail());
         Member member = memberRepository.save(mapper.DTOToEntity(memberDTO));
         return member.getId();
+    }
+
+    private void existsMember(String email) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        if (optionalMember.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+        }
     }
 }

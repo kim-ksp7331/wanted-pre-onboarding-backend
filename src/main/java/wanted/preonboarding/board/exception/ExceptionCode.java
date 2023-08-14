c2e359c1dcda6c3791ab73c4baaa72f03584c17a
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-08-14 10:19:07 +0900

    Fix: 이메일 중복 검사 추가
    로그인시 이메일은 유일해야 하는데 이를 누락했던 것을 수정했습니다.

--- src/main/java/wanted/preonboarding/board/exception/ExceptionCode.java
+++ src/main/java/wanted/preonboarding/board/exception/ExceptionCode.java
@@ -3,8 +3,8 @@ package wanted.preonboarding.board.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-
-    MEMBER_NOT_FOUND(404, "Member Not Found"),;
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
+    MEMBER_EXISTS(409, "Member Exists"),;
     @Getter
     private final int status;
 
